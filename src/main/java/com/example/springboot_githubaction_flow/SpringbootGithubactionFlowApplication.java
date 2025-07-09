50a6b013a2b7c6ba910cdc38a7f2677ed6ceebdc
Sabarish <sabarishkumar.r@gmail.com>
2025-07-09 13:44:19 +0530

    Initial commit

--- src/main/java/com/example/springboot_githubaction_flow/SpringbootGithubactionFlowApplication.java
+++ src/main/java/com/example/springboot_githubaction_flow/SpringbootGithubactionFlowApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootGithubactionFlowApplication {
-
+	/**
+	 * Spring Boot GitHub Action Flow Application
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootGithubactionFlowApplication.class, args);
 	}
